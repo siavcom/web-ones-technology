46f7434059d762e550beac08512c11a1b7e0702f
siavcom <siavcom@hotmail.com>
2025-02-07 16:39:05 -0600

    7 Febrero 2025

--- stores/currentSession.ts
+++ stores/currentSession.ts
@@ -35,7 +35,7 @@ export const Session = defineStore(
     const fileLogoEmp = ref(null);
     const Var = ref(0);
     const menu = ref([]);
-    let socket;
+    let socket: any = ref(null);  // faltaba ref(null)
     const sockets = {};
     let socketIo = ref(false);
     let socketId: string;
@@ -291,7 +291,7 @@ export const Session = defineStore(
         query: "SELECT * from vcomeprg order by NUM_PRG,SIS_SIS,TPR_PRG",
       };
 
-      if (socket) {
+      if (socket.value) {
         // hay socket
         await socket.emit("sql async", dat_vis, (response) => {
           /*
